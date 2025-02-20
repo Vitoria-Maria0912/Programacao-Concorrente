2cf0783a00e41c122a3f2a2fd8598fdd0151ac56
venanciojamilly <jamilly.silva@ccc.ufcg.edu.br>
2025-02-20 17:06:34 -0300

    add: Buffer method size

--- Lab3/src/concurrent/java/Buffer.java
+++ Lab3/src/concurrent/java/Buffer.java
@@ -1,20 +1,45 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Semaphore;
 
 class Buffer {
     private final List<Integer> data = new ArrayList<>();
-    
+    private final Semaphore mutex = new Semaphore(1);
+
     public void put(int value) {
-        data.add(value);
-        System.out.println("Inserted: " + value + " | Buffer size: " + data.size());
+        // região crítica - proteger
+        // ver se o buffer tá cheio
+        try {
+            mutex.acquire();
+            
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+
+        } finally {
+        
+            data.add(value);
+            mutex.release();
+            // região critica - proteger
+            System.out.println("Inserted: " + value + " | Buffer size: " + data.size());
+        }
     }
-    
+
     public int remove() {
         if (!data.isEmpty()) {
+            // região crítica - proteger
+            try {
+
+            mutex.acquire();
+            } 
             int value = data.remove(0);
+            // região crítica - proteger
             System.out.println("Removed: " + value + " | Buffer size: " + data.size());
             return value;
         }
         return -1;
+    }
+
+    public int size(){
+        return this.data.size();
     }
 }
