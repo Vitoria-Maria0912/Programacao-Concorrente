1e356c22b59c241a438753f8467db3e0297e2a1c
venanciojamilly <jamilly.silva@ccc.ufcg.edu.br>
2025-03-20 16:36:05 -0300

    add: files and solution lab6_a1

---
+++ Lab6/lab6_a1.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func producer(ch chan int){
+	rand.Seed(42)
+	for{
+		v := rand.Intn(10)
+		ch <-v //colocando no canal
+	}
+}
+
+func consumer(ch chan int){
+	for v :=  range ch{
+		if(v %2 ==0){
+			fmt.Println("Par:", v)
+		}
+	}
+
+}
+
+func main(){
+
+
+	ch := make(chan int)
+
+	go producer(ch)
+	go consumer(ch)
+
+	select{}
+}
