dee79aa42d3c78eff5a00d23042de2c4a6021680
venanciojamilly <jamilly.silva@ccc.ufcg.edu.br>
2025-03-20 16:44:11 -0300

    add: solution lab6_a2

--- Lab6/lab6_a2.go
+++ Lab6/lab6_a2.go
@@ -5,3 +5,33 @@ import (
 	"math/rand"
 	"time"
 )
+
+func producer(ch chan int) {
+	rand.Seed(42)
+	for i := 0; i < 10000; i++ {
+		v := rand.Intn(100)
+		ch <-v //colocando no canal
+	}
+	close(ch)
+}
+
+func consumer(ch chan int) {
+	for v :=  range ch{
+		if(v %2 ==0){
+			fmt.Println("Par:", v)
+		}
+	}
+
+}
+
+func main() {
+
+
+	ch := make(chan int)
+
+	go producer(ch)
+	go consumer(ch)
+
+	time.Sleep(2 * time.Second) // Tempo suficiente para processar os números
+	
+}
