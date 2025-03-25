129bcf7e160436bb30498e503779c44760b3d2bd
Vitoria-Maria0912 <vitoria.nascimento@ccc.ufcg.edu.br>
2025-03-25 15:46:17 -0300

    feat: sos

--- Lab7/lab7.go
+++ Lab7/lab7.go
@@ -6,9 +6,11 @@ import (
 	"time"
 )
 
-func exec(tempoDormido int) int {
+func exec(maxTime int) int {
 
-	time.Sleep(time.Duration(tempoDormido) * time.Second)
+	tempoDormido := rand.Intn(maxTime)
+
+	time.Sleep(time.Duration(tempoDormido) * time.Second) // time.Milliseconds
 	return tempoDormido
 }
 
@@ -16,17 +18,26 @@ func aux(max_sleep_ms int) chan int {
 
 	channel := make(chan int)
 
-	for i := 0; i <= 1000; i++ {
-		maxTime := rand.Intn(max_sleep_ms)
-		channel <- maxTime
-		exec(maxTime)
-	}
+	go func() {
+		for i := 0; i <= 1000; i++ {
+			tempoDormido := exec(max_sleep_ms)
+			channel <- tempoDormido		
+		} 
+		close(channel)
+	}()
 	return channel
 }
 
-func main(){
+func main() {
 
 	rand.Seed(42)
+	max_sleep_ms1 := rand.Intn(10)
+	max_sleep_ms2 := rand.Intn(10)
 
-	// go aux(max_sleep_ms)
+	for i := 0; i <= 500; i++ {
+		ch1 := aux(max_sleep_ms1)
+		ch2 := aux(max_sleep_ms2)
+		<- ch1
+		<- ch2
+	}
 }
