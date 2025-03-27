8e191d8a6de8cf3a1db22dcbdbb95c133ca472fc
venanciojamilly <jamilly.silva@ccc.ufcg.edu.br>
2025-03-27 17:00:53 -0300

    add: Contador de palavras serial

--- Lab8/src/java/ContadorPalavras.java
+++ Lab8/src/java/ContadorPalavras.java
@@ -3,8 +3,24 @@ import java.util.concurrent.*;
 
 public class ContadorPalavras {
     public static void main(String[] args) {
-	System.out.println("Lab8");    
-
+	System.out.println("Lab8");
+    if (args.length == 0) {
+        System.out.println("Uso: java ContadorPalavras <arquivos>");
+        return;
+    }
+    
+    int totalPalavras = 0;
+    for (String nomeArquivo : args) {
+        try {
+            int count = contarPalavras(nomeArquivo);
+            System.out.println(nomeArquivo + ": " + count + " palavras");
+            totalPalavras += count;
+        } catch (IOException e) {
+            System.err.println("Erro ao ler o arquivo " + nomeArquivo + ": " + e.getMessage());
+        }
+    }
+    
+    System.out.println("Total de palavras: " + totalPalavras);    
     }
 
     static int contarPalavras(String nomeArquivo) throws IOException {
