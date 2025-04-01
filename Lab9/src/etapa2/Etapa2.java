d0d4dbd5bceddc082b79eb03c7089e5b8a501e7b
Vitoria-Maria0912 <vitoria.nascimento@ccc.ufcg.edu.br>
2025-04-01 15:40:29 -0300

    feat: Create etapa2.

---
+++ Lab9/src/etapa2/Etapa2.java
@@ -0,0 +1,60 @@
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Etapa2 {
+
+    private static BlockingQueue<Integer> queue;
+    private static Random random = new Random();
+
+    public Etapa2(Integer size) {
+        queue = new ArrayBlockingQueue<>(size);
+    }
+    
+    public static void producer(Integer n) {
+        
+        try {
+            Thread.sleep(n); 
+            for (int i = 0; i < 10000; i++){
+                Integer nRandom = random.nextInt(10);
+    
+                queue.put(nRandom);
+                System.out.println("Produzindo: "+ nRandom);
+            }
+        } catch (InterruptedException e) {}
+    }
+
+    public static void consumer() {
+
+        try {
+            Thread.sleep(600); 
+            queue.take();
+            System.out.println("Consumindo... ");
+        } catch (InterruptedException e) {}
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Lab9 ...");    
+
+        Integer nInteger = 10;
+
+        ExecutorService executor = Executors.newFixedThreadPool(nInteger);
+
+        List<Future<?>> futures = new ArrayList<>();
+
+        while (true) {
+            Future<?> future = executor.submit(() -> {
+                producer(nInteger);
+                consumer();
+            });
+            futures.add(future);
+        }
+    }
+}
